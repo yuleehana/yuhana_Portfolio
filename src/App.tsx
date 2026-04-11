658bd41c44c3bccc800c9f68a73bd3f0f3a21d1e
yuleehana <yuleehana@gmail.com>
2026-04-11 12:55:06 +0900

    feat: add vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/next';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
+      <Analytics />
     </>
   );
 }
