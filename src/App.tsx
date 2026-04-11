8faa4cb78b7642e9e1c2f4978bc7143c8afb595e
yuleehana <yuleehana@gmail.com>
2026-04-11 13:04:21 +0900

    fix: import 오타 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
